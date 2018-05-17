8ca78bb790805a92c7978ebff5a7f3d47d0bba3b
lcolladotor <lcollado@jhsph.edu>
2018-05-17 11:46:03 -0400

    Added script and regions for Seb (changed seqlevels style to UCSC)

---
+++ seb/seb_regions.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#$ -cwd
+#$ -N seb_regions
+#$ -o ./seb_regions.txt
+#$ -e ./seb_regions.txt
+#$ -m e
+
+## Usage:
+# qsub seb_regions.sh 
+
+bash /users/lcollado/R/x86_64-pc-linux-gnu-library/3.3.x/recount.bwtool/extdata/jhpce/run_rse.sh -r "/dcl01/ajaffe/data/lab/ryten_work/seb/regions_hg38.GR.rda" -s "seb_regions" -c 1
+
